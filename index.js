991c53f1d8ecbc7308216836e9eeb30703259ca0
Alb3rtC001 <albert.corrales@hotmail.com>
2024-05-28 21:26:45 +0200

    develop: Albert - Add logger and execute main page when server wake up

--- index.js
+++ index.js
@@ -1,19 +1,31 @@
 const express = require('express');
 const connectToMongo = require('./config/mongodb');
 const loadRoutes = require('./managerRoutes');
-const bodyParser = require('body-parser'); // Asegúrate de que este middleware está en uso
+const logger = require('./config/wingston');
+const bodyParser = require('body-parser'); // Asegurarse de que este middleware está en uso
+
 require('dotenv').config();
 
 const app = express();
 const routes = loadRoutes(app);
 const PORT = process.env.PORT || 3000;
+const url = `${process.env.TERMINAL_URL}${PORT}/home`;
 
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
+    //logger.info("Init main page");
     connectToMongo();
+    /*import('open').then(open => {
+        open.default(url).catch(err => {
+            Log.info("Init main page");
+            console.error('Failed to open URL:', err);
+        });
+    }).catch(err => {
+        console.error('Failed to load open module:', err);
+    });*/
 });
 
 app.get('/home', function(request, response) {
